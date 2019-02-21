e85c38ac5b4f225300852ec19593190dce2e2fdd
Richard Pentecost <richardpentecost1@gmail.com>
2019-02-21 20:35:50 +0000

    creating a name added to constructor, with test

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,7 +1,19 @@
-const { Pet } = require('../src/pet.js');
+const { Pet } = require('../src/pet');
 
 describe('constructor', () => {
+    
+    let pet = new Pet('Fido');
+    console.log(pet.name);
     it('returns an object', () => {
-        expect(new Pet('Fido')).toBeInstanceOf(Object);
+        expect(pet).toBeInstanceOf(Object);
+    })
+
+    it('pet has a name', () => {
+        expect(pet.name).toEqual('Fido');
     }) 
+
+    // it('checks name', () => {
+    //     const pet = new Pet('Fido');
+    //     expect(pet.name).toBe('Fido');
+    // })
 })
